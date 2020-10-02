c3ebb71ec8d2d48bc3f3c3dab70d9382b6595efb
Tanjina Mumu <46072784+tanjinamumu@users.noreply.github.com>
2020-10-02 12:02:55 +0600

    Add files via upload

---
+++ StudentBatch.java
@@ -0,0 +1,18 @@
+package hellofriends;
+
+/**
+ *
+ * @author Mumu
+ */
+public class StudentBatch 
+{
+    private int batch;
+    public void setBatch(int batch)
+    {
+        this.batch = batch;
+    }
+    public int getBatch()
+    {
+        return batch;
+    }
+}

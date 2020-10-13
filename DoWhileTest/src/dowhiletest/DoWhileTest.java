a827c861f09dee0f71736a0081da8a3819d8edb7
Tanjina Mumu <46072784+tanjinamumu@users.noreply.github.com>
2020-10-13 08:43:21 +0600

    Add files via upload

---
+++ DoWhileTest/src/dowhiletest/DoWhileTest.java
@@ -0,0 +1,19 @@
+package dowhiletest;
+
+/**
+ *
+ * @author Mumu
+ */
+public class DoWhileTest 
+{
+    public static void main(String[] args) 
+    {
+        int counter = 1; 
+        do
+        {
+            System.out.printf("%d ", counter);
+            ++counter;
+        } while (counter <= 10); 
+        System.out.println(); 
+    }
+}

8c80305e43c9d6cfedc22d390dbcb255bc4a7a35
Tanjina Mumu <46072784+tanjinamumu@users.noreply.github.com>
2020-10-13 08:45:07 +0600

    Add files via upload

---
+++ ForCounter/src/forcounter/ForCounter.java
@@ -0,0 +1,17 @@
+package forcounter;
+
+/**
+ *
+ * @author Mumu
+ */
+public class ForCounter 
+{
+    public static void main(String[] args) 
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.printf("%d ",i);
+        }     
+        System.out.println();
+    }
+}

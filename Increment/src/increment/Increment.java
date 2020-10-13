38045c054d7ef9f31a69bdaca14ef2504f9fcb83
Tanjina Mumu <46072784+tanjinamumu@users.noreply.github.com>
2020-10-13 08:46:34 +0600

    Add files via upload

---
+++ Increment/src/increment/Increment.java
@@ -0,0 +1,27 @@
+package increment;
+
+/**
+ *
+ * @author Mumu
+ */
+public class Increment 
+{
+    public static void main(String[] args) 
+    {
+        // Prefix increment and postfix increment operators.
+        
+       int c = 5;
+       System.out.printf(" c before postincrement: %d\n", c); // prints 5
+       System.out.printf("     preincrementing c: %d\n", c++); // prints 6
+       System.out.printf("  c after preincrement: %d\n", c); // prints 6 
+       System.out.println(); // skip a line
+
+        // demonstrate prefix increment operator
+        c = 5;
+        System.out.printf(" c before preincrement: %d\n", c); // prints 5
+        System.out.printf("    postincrementing c: %d\n",++c); // prints 5
+        System.out.printf(" c after postincrement: %d\n", c); // prints 6
+        
+    }
+    
+}

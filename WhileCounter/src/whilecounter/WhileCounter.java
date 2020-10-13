9cfd0537f7b8f078101aa97af7488bceb5490008
Tanjina Mumu <46072784+tanjinamumu@users.noreply.github.com>
2020-10-13 08:52:10 +0600

    Add files via upload

---
+++ WhileCounter/src/whilecounter/WhileCounter.java
@@ -0,0 +1,20 @@
+package whilecounter;
+
+/**
+ *
+ * @author Mumu
+ */
+public class WhileCounter 
+{
+    public static void main(String[] args) 
+    {
+        int counter=1;
+        while(counter<=100)
+        {
+            System.out.printf("%d ",counter);
+            counter++;
+        }
+        System.out.println();
+    }
+    
+}
